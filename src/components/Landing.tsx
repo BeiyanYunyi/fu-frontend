d929529f97b47d70414fbfebb215ac751e68dc9e
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-04-26 17:18:46 +0800

    🎨

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,12 +2,10 @@ import type { Component } from 'solid-js';
 
 const Landing: Component = () => (
   <div class="h-[100vh] flex flex-col">
-    <div class="flex-grow" />
-    <div class="m-auto flex flex-col items-center">
+    <div class="m-auto flex flex-col items-center justify-center flex-grow">
       <h1 class="text-5xl line-height-normal">Night has fallen</h1>
       <h2 class="text-3xl text-[#b7b7b7]">Protect yourself</h2>
     </div>
-    <div class="flex-grow" />
     <div class="nm-flat p-8 m-4 rounded-xl">
       <h1 class="text-3xl text-center">机构列表</h1>
     </div>
