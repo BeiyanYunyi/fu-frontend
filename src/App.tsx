a82b443768584952330e703e51871b14830ef696
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-04-27 19:31:54 +0800

    🎨

--- src/App.tsx
+++ src/App.tsx
@@ -15,45 +15,45 @@ const App: Component = () => {
         <For each={data()}>
           {(item) => (
             <li class="nm-flat p-4 m-4 rounded-xl">
-              <h2 class="text-xl">{item.name}</h2>
+              <For each={item.names}>{(name) => <h2 class="text-xl">{name}</h2>}</For>
               <div class="flex flex-col gap-2 my-2 break-all">
                 <div>
                   <p>网址</p>
-                  <p>{item.website}</p>
+                  <For each={item.sites}>{(site) => <p>{site}</p>}</For>
                 </div>
-                <div>
-                  <p>地址</p>
-                  <p>{item.location}</p>
-                </div>
+                <Show when={item.locations[0] && item.locations[0] !== 'UNKNOWN'}>
+                  <div>
+                    <p>地址</p>
+                    <For each={item.locations}>{(location) => <p>{location}</p>}</For>
+                  </div>
+                </Show>
                 <div>
                   <p>来源</p>
-                  <p>{item.source}</p>
+                  <For each={item.sources}>{(source) => <p>{source}</p>}</For>
                 </div>
               </div>
-              <Show when={item.persecutingTrans}>
-                <p>可能存在对跨性别者的迫害</p>
+              <Show when={item.persecution.known}>
+                <p class="text-red-400">可能存在对跨性别者的迫害</p>
               </Show>
-              <Show when={item.factChecking}>
+              <Show when={!item.checked}>
                 <p>待审核</p>
               </Show>
-              <Show when={item.proof}>
-                <ul>
-                  <For each={item.proof}>
-                    {(item2) => (
-                      <li>
-                        <a
-                          href={item2}
-                          rel="noreferrer noopener"
-                          target="_blank"
-                          class="text-blue underline"
-                        >
-                          相关证据
-                        </a>
-                      </li>
-                    )}
-                  </For>
-                </ul>
-              </Show>
+              <ul>
+                <For each={item.persecution.evidences}>
+                  {(evidence) => (
+                    <li>
+                      <a
+                        href={evidence}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                        class="text-blue underline"
+                      >
+                        相关证据
+                      </a>
+                    </li>
+                  )}
+                </For>
+              </ul>
             </li>
           )}
         </For>
--- src/utils/getList.ts
+++ src/utils/getList.ts
@@ -1,27 +1,17 @@
-type Row = [string, string, string, string, string, string, string, string];
+interface Data {
+  names: string[];
+  sites: string[];
+  locations: string[];
+  persecution: { known: boolean; evidences: string[] };
+  sources: string[];
+  checked: boolean;
+}
 
-const getList = async () => {
-  const txt = await (
+const getList = async () =>
+  (await (
     await fetch(
-      'https://raw.githubusercontent.com/FunctionSir/TransDefenseProject/master/%E6%88%92%E7%BD%91%E7%98%BE(%E5%8F%AF%E8%83%BD%E5%90%AB%E6%89%AD%E8%BD%AC%E6%B2%BB%E7%96%97)%E6%9C%BA%E6%9E%84%E5%88%97%E8%A1%A8.csv',
+      'https://raw.githubusercontent.com/FunctionSir/TransDefenseProject/master/institute_list.json',
     )
-  ).text();
-  const table: Row[] = txt
-    .split('\n')
-    .map((e) => e.split(',') as Row)
-    .slice(1);
-  table.pop();
-  const data = table.map((row) => ({
-    id: row[0],
-    name: row[1],
-    website: row[2],
-    location: row[3],
-    source: row[4],
-    persecutingTrans: row[5] === 'TRUE',
-    proof: row[6] === 'NONE' || row[6] === 'PENDING' ? null : row[6].split(' || '),
-    factChecking: row[7] === 'TRUE',
-  }));
-  return data;
-};
+  ).json()) as Data[];
 
 export default getList;
