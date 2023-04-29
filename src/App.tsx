c476413bef24e4d01f129d4db6949ad8e375beab
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-04-30 01:48:32 +0800

    ⚡️ Replace unocss with style9

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import { Component, For, Show, createResource } from 'solid-js';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@unocss/reset/tailwind-compat.css';
-import 'virtual:uno.css';
+import style9 from 'style9';
 import './App.css';
 import Landing from './components/Landing';
 import getList from './utils/getList';
+
+const styles = style9.create({
+  nmFlat: {
+    backgroundImage: 'linear-gradient(145deg, rgba(115, 115, 115, 0.15) 15%, rgb(43, 45, 47) 80%);',
+    boxShadow: '20px 20px 60px #161718, -20px -20px 60px #404346',
+  },
+  institutionListItem: {
+    padding: '1rem',
+    margin: '1rem',
+    borderRadius: '0.75rem',
+  },
+  textXl: { fontSize: '1.25rem', lineHeight: '1.75rem' },
+  institutionListContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.5rem',
+    marginTop: '0.5rem',
+    marginBottom: '0.5rem',
+    wordBreak: 'break-all',
+  },
+  textRed400: { color: 'rgb(248, 113, 113)' },
+  linkText: { color: 'rgb(96, 165, 250)', textDecorationLine: 'underline' },
+});
 
 const App: Component = () => {
   const [data] = createResource(getList);
@@ -14,9 +37,9 @@ const App: Component = () => {
       <ul>
         <For each={data()}>
           {(item) => (
-            <li class="nm-flat p-4 m-4 rounded-xl">
-              <For each={item.names}>{(name) => <h2 class="text-xl">{name}</h2>}</For>
-              <div class="flex flex-col gap-2 my-2 break-all">
+            <li class={styles('institutionListItem', 'nmFlat')}>
+              <For each={item.names}>{(name) => <h2 class={styles('textXl')}>{name}</h2>}</For>
+              <div class={styles('institutionListContainer')}>
                 <div>
                   <p>网址</p>
                   <For each={item.sites}>{(site) => <p>{site}</p>}</For>
@@ -33,7 +56,7 @@ const App: Component = () => {
                 </div>
               </div>
               <Show when={item.persecution.known}>
-                <p class="text-red-400">可能存在对跨性别者的迫害</p>
+                <p class={styles('textRed400')}>可能存在对跨性别者的迫害</p>
               </Show>
               <Show when={!item.checked}>
                 <p>待审核</p>
@@ -46,7 +69,7 @@ const App: Component = () => {
                         href={evidence}
                         rel="noreferrer noopener"
                         target="_blank"
-                        class="text-blue underline"
+                        class={styles('linkText')}
                       >
                         相关证据
                       </a>
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,47 @@
 import type { Component } from 'solid-js';
+import style9 from 'style9';
+
+const styles = style9.create({
+  landingContainer: { height: '100vh', display: 'flex', flexDirection: 'column' },
+  titleContainer: {
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: '2.25rem',
+    '@media (min-width: 768px)': {
+      fontSize: '3rem',
+    },
+    lineHeight: 1.5,
+  },
+  subtitle: {
+    fontSize: '1.5rem',
+    '@media (min-width: 768px)': { fontSize: '1.875rem' },
+    color: '#b7b7b7',
+  },
+  instListTitleContainer: {
+    backgroundImage: 'linear-gradient(145deg, rgba(115, 115, 115, 0.15) 15%, rgb(43, 45, 47) 80%);',
+    boxShadow: '20px 20px 60px #161718, -20px -20px 60px #404346',
+    padding: '2rem',
+    margin: '1rem',
+    borderRadius: '0.75rem',
+  },
+  instListTitle: { fontSize: '1.875rem', textAlign: 'center', lineHeight: 1.2 },
+});
 
 const Landing: Component = () => (
-  <div class="h-[100vh] flex flex-col">
-    <div class="m-auto flex flex-col items-center justify-center flex-grow">
-      <h1 class="text-4xl md:text-5xl line-height-normal">Night has fallen</h1>
-      <h2 class="text-2xl md:text-3xl text-[#b7b7b7] mt-4">Protect yourself</h2>
+  <div class={styles('landingContainer')}>
+    <div class={styles('titleContainer')}>
+      <h1 class={styles('title')}>Night has fallen</h1>
+      <h2 class={styles('subtitle')}>Protect yourself</h2>
     </div>
-    <div class="nm-flat p-8 m-4 rounded-xl">
-      <h1 class="text-3xl text-center">机构列表</h1>
+    <div class={styles('instListTitleContainer')}>
+      <h1 class={styles('instListTitle')}>机构列表</h1>
     </div>
   </div>
 );
